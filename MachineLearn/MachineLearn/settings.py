0f55f939d2f0dbd875f3579041af1dd44a6f4867
rpasricha45 <rpasricha45@gmail.com>
2018-08-07 20:08:29 -0400

    complete update to design

--- MachineLearn/MachineLearn/settings.py
+++ MachineLearn/MachineLearn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'jhtv354e%(&hi22k0y@2tqmwn)vi9a$#9*leu5&)b2w*oaa1l3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
